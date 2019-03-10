a40dab6017e92570b429892ff654ed8e11d7f422
Savannah C <sgcorrell@gmail.com>
2019-03-10 16:50:45 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,14 +1,14 @@
 def dictionary
   words = {
-    "hello" => "hi"
-    "to" => "2"
-    "two" => "2"
-    "too" => "2"
-    "for" => "4"
-    "four" => "4"
-    "be" => "b"
-    "you" => "u"
-    "at" => "@"
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
     "and" => "&"
   }
 end
